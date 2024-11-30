53cd47c324f403bb5737bd91dffb55fc523c336b
Juanperias <136520331+Juanperias@users.noreply.github.com>
2024-11-30 11:54:13 -0400

    init: dev

---
+++ kernel/src/main.rs
@@ -0,0 +1,64 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
+use limine::BaseRevision;
+
+#[used]
+#[link_section = ".requests"]
+static BASE_REVISION: BaseRevision = BaseRevision::new();
+
+#[used]
+#[link_section = ".requests"]
+static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[link_section = ".requests_start_marker"]
+static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
+#[used]
+#[link_section = ".requests_end_marker"]
+static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
+
+#[no_mangle]
+extern "C" fn main() -> ! {
+    assert!(BASE_REVISION.is_supported());
+
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
+        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
+            let width = framebuffer.width() as u64;
+            let height = framebuffer.height() as u64;
+
+            for y in 0..height {
+                for x in 0..width {
+                    let pixel_offset = y * framebuffer.pitch() + x * 4;
+
+                    unsafe {
+                        *(framebuffer.addr().add(pixel_offset as usize) as *mut u32) = 0xFFFFFFFF;
+                    }
+                }
+            }
+        }
+    }
+
+    hcf();
+}
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    hcf();
+}
+
+fn hcf() -> ! {
+    loop {
+        unsafe {
+            #[cfg(target_arch = "x86_64")]
+            asm!("hlt");
+            #[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
+            asm!("wfi");
+            #[cfg(target_arch = "loongarch64")]
+            asm!("idle 0");
+        }
+    }
+}
